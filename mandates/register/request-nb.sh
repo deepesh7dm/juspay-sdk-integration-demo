dd7a00ed00def4cb46120a28a6ddac4afdb4dede
Chinthala Shoukthik Sai <chinthala.shoukthiksai@juspay.in>
2023-09-27 19:20:26 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference-global>mandates/register/request-nb.sh @ paytm-gateway-configuration/docs/mandate-registration-api by Chinthala Shoukthik Sai

--- mandates/register/request-nb.sh
+++ mandates/register/request-nb.sh
@@ -3,8 +3,8 @@ curl -X POST 'https://api.juspay.in/txns' \
 -d 'order_id=ORD1622098688' \
 -d 'merchant_id=guest' \
 -d 'payment_method_type=NB' \
--d 'payment_method=JP_HDFC' \
--d 'bank_ifsc=HDFC0000053' \
+-d 'payment_method=JP_BCA' \
+-d 'bank_ifsc=BCA0000053' \ (need to be confirmed what is ifsc alternative in indonesia )
 -d 'bank_account_number=50100013132000' \
 -d 'bank_beneficiary_name=test' \
 -d 'should_create_mandate=true' \
